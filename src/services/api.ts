0892b919f2795308558b9a224c4120fa45350ad6
Igor Amantino <justin.amantino@gmail.com>
2024-01-22 15:08:16 -0300

    fix/router url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,23 +1,12 @@
 
 import axios from "axios";
- const BaseUrl = "http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon"
 
 
 
 export const api = axios.create({
-  baseURL: 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
+  baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
 });
 
 
 
 
-export async function fetchDragons() {
-  try {
-    const dragons =  fetch(BaseUrl).then(response => response.json());
-
-    return dragons
-  }
-  catch (error) {
-    console.log(error)
-  }
-}
