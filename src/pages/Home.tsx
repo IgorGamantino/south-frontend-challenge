4e8db3453f62b6e3c211be7d2ecd454467de21b2
Igor Amantino <justin.amantino@gmail.com>
2024-01-23 11:33:23 -0300

    feat/text-no-result

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { ModalCreatedDragon } from "../components/ModalCreatedDragon";
 import { InputSearch } from "../components/InputSearch";
 
 export function Home() {
-  const { setListDragon,filteredListDragon } = useContext(ListDragonContext);
+  const { setListDragon, filteredListDragon } = useContext(ListDragonContext);
 
   useEffect(() => {
     async function getDragonList() {
@@ -15,7 +15,6 @@ export function Home() {
     }
     getDragonList();
   }, [setListDragon]);
-
 
   return (
     <main className="min-h-screen w-screen  flex  p-2 sm:p-10 justify-center">
@@ -35,18 +34,25 @@ export function Home() {
         </div>
 
         <div className="mt-20 flex items-center bg-black400 flex-wrap gap-2 md:gap-4 justify-center w-full  py-2">
-          {filteredListDragon()
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .map((dragon) => (
-              <DragonCard
-                key={dragon.id}
-                name={dragon.name}
-                createdAt={dragon.createdAt}
-                histories={dragon.histories}
-                type={dragon.type}
-                id={dragon.id}
-              />
-            ))}
+          {filteredListDragon().length > 0 &&
+            filteredListDragon()
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((dragon) => (
+                <DragonCard
+                  key={dragon.id}
+                  name={dragon.name}
+                  createdAt={dragon.createdAt}
+                  histories={dragon.histories}
+                  type={dragon.type}
+                  id={dragon.id}
+                />
+              ))}
+
+          {filteredListDragon().length <= 0 && (
+            <span className="text-white font-bold text-2xl">
+              não temos nenhum dragão na lista
+            </span>
+          )}
         </div>
       </section>
     </main>
