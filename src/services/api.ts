f8ef2fb68557875a32e3ed05ef8b8ccc74d60500
Igor Amantino <justin.amantino@gmail.com>
2024-01-23 11:33:59 -0300

    fix/routerHttps

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
+  baseURL: 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
 });
 
 
